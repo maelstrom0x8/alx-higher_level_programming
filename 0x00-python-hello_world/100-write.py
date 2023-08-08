67084295157548a231baf8c8059fc11941b0a416
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-08-08 02:48:40 +0100

    minor changes

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-from sys import stdout
-stdout.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+import sys
+sys.stdout.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)
