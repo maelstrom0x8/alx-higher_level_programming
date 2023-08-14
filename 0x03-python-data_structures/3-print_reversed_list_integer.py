cd6c58efa639024465e50d7f8e12d2faf00bab1c
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-08-14 21:48:45 +0100

    Hotfixes for failing test

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
+    if not my_list:
+        return
     list_ln = len(my_list) - 1
     for i in range(list_ln, -1, -1):
         print("{:d}".format(my_list[i]), end="\n")
