41651d71af1af996549e4ed1fc5a0f0396fe4c16
Emmanuel Godwin <emmanuel.eze.godwin@gmail.com>
2023-12-01 19:31:23 +0100

    Minor changes

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # Sends a request to that URL, and displays the size of the body of the response
-url=$1
-size=$(curl -sI "$url" | grep -i 'content-length' | awk '{print $2}' | tr -d '\r\n')
-
-if [ -n "$size" ]; then
-    echo "${size}"
-fi
+curl -sI "$1" | grep -i 'content-length' | awk '{print $2}' | tr -d '\r\n'
